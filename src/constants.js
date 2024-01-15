2466215b433e629327417a1bccffad37ea394566
unknown <DIVIGTI@DIVIGTI-732737.sesp.ac.gov.br>
2024-01-15 14:50:51 -0500

    Url

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const NODE_ENV = process.env.NODE_ENV || "development";
 
-const URL_API = NODE_ENV === "production" ? "" : "http://localhost:3003";
+const URL_API = NODE_ENV === "production" ? "https://estatisticas-sejusp-node-estatisticas-sejusp.apps.okd.sejusp/" : "http://localhost:3003";
 
 export const instance = axios.create({
   baseURL: URL_API,
